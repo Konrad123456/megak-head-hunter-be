58f45e794263074b15c5c3b4d501a0b1f4c04c91
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-04-20 21:07:33 +0200

    Added entity students_rating

---
+++ src/entities/studentsRating/studentsRating.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, BaseEntity, PrimaryColumn } from 'typeorm';
+import { StudnetsRating } from '../types/studentsRating';
+
+@Entity('students_rating')
+export class StudentsData extends BaseEntity implements StudnetsRating {
+  @PrimaryColumn()
+  id: string;
+
+  @Column({ type: 'tinyint', length: 1 })
+  courseCompletion: number;
+
+  @Column({ type: 'tinyint', length: 1 })
+  courseEngagment: number;
+
+  @Column({ type: 'tinyint', length: 1 })
+  projectDegree: number;
+
+  @Column({ type: 'tinyint', length: 1 })
+  teamProjectDegree: number;
+
+  @Column({ type: 'simple-array' })
+  bonusProjectUrls: string;
+}
