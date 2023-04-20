a04f03c1b915b411ce9b9f8ccba6e6703741e0ff
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-04-20 22:26:16 +0200

    Corrections after CR.

--- src/entities/hr/hr.entity.ts
+++ src/entities/hr/hr.entity.ts
@@ -6,12 +6,12 @@ export class Hr extends BaseEntity implements hrInterface {
   @PrimaryColumn()
   id: string;
 
-  @Column({ type: 'string', length: 255, })
+  @Column({ type: 'varchar', length: 255 })
   fullName: string;
 
-  @Column({ type: 'string', length: 255, })
+  @Column({ type: 'varchar', length: 255 })
   company: string;
 
-  @Column({ type: 'smallint', length: 3, })
+  @Column({ type: 'smallint', length: 3 })
   maxReservedStudents: number;
 }
