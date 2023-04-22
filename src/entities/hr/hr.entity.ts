9644b3b767b70f18aa8bb3d73a93d99c50c4ef35
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-04-22 09:53:29 +0200

    Added relation one to one User-Hr.

--- src/entities/hr/hr.entity.ts
+++ src/entities/hr/hr.entity.ts
@@ -1,10 +1,14 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, PrimaryColumn } from 'typeorm';
+import { Entity, Column, BaseEntity, PrimaryColumn, OneToOne, JoinColumn } from 'typeorm';
 import { hrInterface } from '../types/hr';
+import { User } from '../User/User.entity';
 
 @Entity('hr')
 export class Hr extends BaseEntity implements hrInterface {
   @PrimaryColumn()
   id: string;
+  @OneToOne(() => User, { 'cascade': true })
+  @JoinColumn({ name: 'id' })
+  user: User;
 
   @Column({ type: 'varchar', length: 255 })
   fullName: string;
