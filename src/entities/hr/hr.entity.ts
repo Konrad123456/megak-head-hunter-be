4b069125ac6a20d809674bd2552623c529984164
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-04-25 02:27:11 +0200

    Added validation to hr.

--- src/entities/hr/hr.entity.ts
+++ src/entities/hr/hr.entity.ts
@@ -1,17 +1,35 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, PrimaryColumn } from 'typeorm';
+import { Entity, Column, BaseEntity, PrimaryColumn, OneToOne, JoinColumn } from 'typeorm';
 import { hrInterface } from '../types/hr';
+import { IsInt, Length, Min, Max } from 'class-validator';
+import { User } from '../User/User.entity';
 
 @Entity('hr')
 export class Hr extends BaseEntity implements hrInterface {
   @PrimaryColumn()
   id: string;
+  @OneToOne(() => User, { 'cascade': true })
+  @JoinColumn({ name: 'id' })
+  user: User;
 
   @Column({ type: 'varchar', length: 255 })
+  @Length(3, 255, {
+    message: '$property: Minimal length is $constraint1 characters and maximal lenght is $constraint2, but actual is $value.'
+  })
   fullName: string;
 
   @Column({ type: 'varchar', length: 255 })
+  @Length(3, 255, {
+    message: '$property: Minimal length is $constraint1 characters and maximal lenght is $constraint2, but actual is $value.'
+  })
   company: string;
 
-  @Column({ type: 'smallint', length: 3 })
+  @Column({ type: 'smallint' })
+  @IsInt()
+  @Min(1, {
+    message: '$property: Minimal value is $constraint1.'
+  })
+  @Max(999, {
+    message: '$property: Maximal value is $constraint1.'
+  })
   maxReservedStudents: number;
 }
