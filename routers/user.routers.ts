4adbab2d5dd9657c790830f83f2aaf09ada3f104
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-04-29 14:19:55 +0200

    Added router get one student.

--- routers/user.routers.ts
+++ routers/user.routers.ts
@@ -1,26 +1,36 @@
 import { Router } from 'express';
 import { getStudentsList } from './user/getStudentsList';
-import { getStudentsCV } from './user/getStudentsCV';
+import { getStudentCV } from './user/getStudentCV';
+import { getOneStudent } from './user/getOneStudent';
 
 export const userRouter = Router()
   // Student CV
   // Example url: http://localhost:3001/user/cv/0c43b548-8b89-4931-a8a2-715d8f531ecd
   .get('/cv/:id', async (req, res, next) => {
-    getStudentsCV(req, res, next);
+    getStudentCV(req, res, next);
   })
-
+  
   // Students list
   // Example url: http://localhost:3001/user/2/10
   .get('/:page/:limit', async (req, res, next) => {
     getStudentsList(req, res, next);
   })
-
+  
+  // Student CV
+  // Example url: http://localhost:3001/user/0c43b548-8b89-4931-a8a2-715d8f531ecd
+  .get('/:id', async (req, res, next) => {
+    // add new user
+    getOneStudent(req, res, next);
+  })
+  
   .post('/:id', async (req, res, next) => {
     // add new user
+    res.json(`Post user: ${req.params.id}`);
   })
 
   .put('/:id', async (req, res, next) => {
     // update students or hr data
+    res.json(`Put user: ${req.params.id}`);
   })
 
   .get('/talk/', async (req, res, next) => {
---
+++ routers/user/getOneStudent.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import { Roles } from "../../src/entities/types/Roles";
+import { ValidationError } from "../../utils/errorsHandler";
+import { StudentsRating } from "../../src/entities/studentsRating/studentsRating.entity";
+import { myDataSource } from "../../config/database.configuration";
+import { StudentStatus } from "../../src/entities/types/studentsData";
+import { UserPayloadData } from "../../utils/createTokens";
+import { DestructuringToGetOne } from "../../utils/destructuringGetOne";
+
+type RequestAndPayloadUser = Request & UserPayloadData;
+
+export const getOneStudent = async (req: Request, res: Response, next: NextFunction) => {
+  const { id, role } = req.user as RequestAndPayloadUser
+
+  if (role !== Roles.HR && role !== Roles.STUDENT) throw new ValidationError('Access denied.', 401);
+
+  const userId = req.params.id;
+
+  const results = await myDataSource
+    .getRepository(StudentsRating)
+    .createQueryBuilder('studentsRating')
+    .leftJoinAndSelect('studentsRating.user', 'user')
+    .leftJoinAndSelect('studentsRating.studentsData', 'studentsData')
+    .where(`user.role = '${Roles.STUDENT}'`)
+    .andWhere(`studentsData.status = '${StudentStatus.AVAILABLE}'`)
+    .andWhere('studentsRating.id = :userId', { userId })
+    .orderBy('studentsData.lastName, studentsData.firstName')
+    .getOne();
+
+  if (!results) return res.json([]);
+
+  const data = {
+    ...results,
+    ...results.user,
+    ...results.studentsData,
+  }
+
+  const userCV = new DestructuringToGetOne(data).returnData();
+
+  res.json(userCV);
+}
