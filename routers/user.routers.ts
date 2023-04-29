2ea2c21158e59a79b08a42a9c8f1e54863163d54
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-04-29 18:21:11 +0200

    Added get number of students to pagination.

--- routers/user.routers.ts
+++ routers/user.routers.ts
@@ -2,27 +2,39 @@ import { Router } from 'express';
 import { getStudentsList } from './user/getStudentsList';
 import { getStudentCV } from './user/getStudentCV';
 import { getOneStudent } from './user/getOneStudent';
+import { getAllStudentsCount } from './user/getAllStudentsCount';
 
 export const userRouter = Router()
+  // Get students count
+  // Example url: http://localhost:3001/user/count
+  .get('/count', async (req, res, next) => {
+    getAllStudentsCount(req, res, next);
+  })
+
   // Student CV
   // Example url: http://localhost:3001/user/cv/0c43b548-8b89-4931-a8a2-715d8f531ecd
   .get('/cv/:id', async (req, res, next) => {
     getStudentCV(req, res, next);
   })
-  
+
   // Students list
   // Example url: http://localhost:3001/user/2/10
   .get('/:page/:limit', async (req, res, next) => {
     getStudentsList(req, res, next);
   })
-  
+
   // Student CV
   // Example url: http://localhost:3001/user/0c43b548-8b89-4931-a8a2-715d8f531ecd
   .get('/:id', async (req, res, next) => {
     // add new user
     getOneStudent(req, res, next);
   })
-  
+
+  .get('/talk/', async (req, res, next) => {
+    // Students to talk
+    res.json(`Get to talk.`);
+  })
+
   .post('/:id', async (req, res, next) => {
     // add new user
     res.json(`Post user: ${req.params.id}`);
@@ -33,7 +45,7 @@ export const userRouter = Router()
     res.json(`Put user: ${req.params.id}`);
   })
 
-  .get('/talk/', async (req, res, next) => {
-    // Students to talk
+  .put('/password/:id', async (req, res, next) => {
+    // update students or hr data
+    res.json(`Put pass: ${req.params.id}`);
   })
-
