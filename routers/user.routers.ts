f38ea0e3d4587f87d03fdbc7cb75564735bf1fc1
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-04-29 12:24:32 +0200

    Changing the order of paths.

--- routers/user.routers.ts
+++ routers/user.routers.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 import { getStudentsList } from './user/getStudentsList';
+import { getStudentsCV } from './user/getStudentsCV';
 
 export const userRouter = Router()
+  // Student CV
+  // Example url: http://localhost:3001/user/cv/0c43b548-8b89-4931-a8a2-715d8f531ecd
+  .get('/cv/:id', async (req, res, next) => {
+    getStudentsCV(req, res, next);
+  })
+
+  // Students list
+  // Example url: http://localhost:3001/user/2/10
   .get('/:page/:limit', async (req, res, next) => {
-    // Students list
-    // Example url http://localhost:3001/user/2/10
     getStudentsList(req, res, next);
   })
 
@@ -16,10 +23,7 @@ export const userRouter = Router()
     // update students or hr data
   })
 
-  .get('/cv/:id', async (req, res, next) => {
-    // Student CV
-  })
-
   .get('/talk/', async (req, res, next) => {
     // Students to talk
   })
+
