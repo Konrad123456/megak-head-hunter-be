773d70b438e1ed7aea95f723aa9c11f16f35810f
Konrad Cencelewicz <konrad.cencelewicz@gmail.com>
2023-05-03 10:22:52 +0200

    [Fixed register route] Added validation for registerToken and now we sent id not email

--- language/en.pl.ts
+++ language/en.pl.ts
@@ -3,10 +3,18 @@ export const staticText = {
         SupportFilesCsvJson: 'Nie wspierany typ pliku. Wspieramy tylko CSV/JSON',
     },
     validation: {
+        user: {
+          invalidToken: 'Invalid register token',
+        },
         UserDoesntExist: 'Podany użytkownik nie istnieje.',
+        WrongRegisterToken: 'Podany użytkownik nie istnieje.',
         AccessDenied: 'Dostęp zabroniony.',
         UnauthorizeAccess: 'Nieautoryzowany dostęp.',
-        WrrongPassword: 'Niepoprawny login lub hasło.',
+        WrongPassword: 'Niepoprawny login lub hasło.',
         EmailDoesntExist: 'Podany amail nie istnieje.',
+        password: {
+            toShort: 'Hasło musi zawierać od 8 do 20 znaków. Hasło powinno zawierać małe i wielkie litery, cyfrę i znak specjalny.',
+            confirmBeTheSame: 'Podane hasła muszą być takie same.',
+        }
     }
 }
