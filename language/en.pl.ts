36770a6ff4f98bf96df57f98b694581b1b00ad95
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-05-03 21:01:01 +0200

    New static text.

--- language/en.pl.ts
+++ language/en.pl.ts
@@ -4,14 +4,16 @@ export const staticText = {
     },
     validation: {
         user: {
-          invalidToken: 'Invalid register token',
+          invalidToken: 'Invalid register token.',
         },
+        Unauthorized: 'Nieautoryzowany dostęp.',
         UserDoesntExist: 'Podany użytkownik nie istnieje.',
         WrongRegisterToken: 'Podany użytkownik nie istnieje.',
         AccessDenied: 'Dostęp zabroniony.',
         UnauthorizeAccess: 'Nieautoryzowany dostęp.',
         WrongPassword: 'Niepoprawny login lub hasło.',
         EmailDoesntExist: 'Podany amail nie istnieje.',
+        unconfirmedAccount: 'Konto nie zostało powtierdzone.',
         password: {
             toShort: 'Hasło musi zawierać od 8 do 20 znaków. Hasło powinno zawierać małe i wielkie litery, cyfrę i znak specjalny.',
             confirmBeTheSame: 'Podane hasła muszą być takie same.',
