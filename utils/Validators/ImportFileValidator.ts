60fc6144492ff7ae2fd4ec81a46762c0b6885f17
Konrad Cencelewicz <konrad.cencelewicz@gmail.com>
2023-05-04 20:23:02 +0200

    [Added validation for import data] added rest of validation for import data

--- utils/Validators/ImportFileValidator.ts
+++ utils/Validators/ImportFileValidator.ts
@@ -1,5 +1,5 @@
 import {StudentsRatingWithEmail} from "../../src/entities/types/studentsRating";
-import {IsEmail, IsNotEmpty, validateSync, ValidationArguments} from "class-validator";
+import {IsEmail, IsNotEmpty, Max, Min, validateSync, ValidationArguments} from "class-validator";
 import {ValidationError} from "../errorsHandler";
 import {staticText} from "../../language/en.pl";
 
@@ -12,22 +12,47 @@ export class ImportFileValidator {
     })
     private email: string;
 
+    @Min(1)
+    @Max(5)
+    private courseCompletion: number
+
+    @Min(1)
+    @Max(5)
+    private courseEngagment: number;
+
+    @Min(1)
+    @Max(5)
+    private projectDegree: number;
+
+    @Min(1)
+    @Max(5)
+    private teamProjectDegree: number;
+
+    @IsNotEmpty()
+    private bonusProjectUrls: string[];
+
     constructor(private userData: StudentsRatingWithEmail) {
         this.email = userData.email;
+        this.courseCompletion = Number(userData.courseCompletion);
+        this.courseEngagment = Number(userData.courseEngagment);
+        this.projectDegree = Number(userData.projectDegree);
+        this.teamProjectDegree = Number(userData.teamProjectDegree);
+        this.bonusProjectUrls = userData.bonusProjectUrls;
         this.validate();
     }
 
     protected validate() {
         const errors = validateSync(this);
-        if (errors.length > 0) {
-            const chekit = Object.values(errors[0]);
-            throw new ValidationError('error', 422);
+        if (errors.length > 0 && errors[0].constraints) {
+            throw new ValidationError(this.errorsToString(errors[0].constraints), 422);
         }
     }
 
-    private errorsToString(errors: ValidationError[]) {
-        // return errors.reduce((prev, curr) => {
-        //     // return prev + ', ' + curr[0].constraints.join(', ');
-        // }, '')
+    private errorsToString(errors: {[p: string]: string}): string {
+        let errorsMessage = '';
+        for (const error in errors) {
+            errorsMessage += `${errors[error]}, `;
+        }
+        return errorsMessage;
     }
 }
