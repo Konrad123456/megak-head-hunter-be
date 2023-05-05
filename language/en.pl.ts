0f1c12c427188b6737525483cbfad38fa0688cea
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-05-05 12:25:28 +0200

    Added static texts.

--- language/en.pl.ts
+++ language/en.pl.ts
@@ -1,20 +1,33 @@
 export const staticText = {
     errors: {
-        SupportFilesCsvJson: 'Nie wspierany typ pliku. Wspieramy tylko CSV/JSON',
+        SupportFilesCsvJson: 'Nie wspierany typ pliku. Wspieramy tylko CSV/JSON.',
+        InternalServerError: 'Wewnętrzny błąd serwera.',
     },
     validation: {
         user: {
-          invalidToken: 'Invalid register token',
+          invalidToken: 'Invalid register token.',
         },
-        UserDoesntExist: 'Podany użytkownik nie istnieje.',
-        WrongRegisterToken: 'Podany użytkownik nie istnieje.',
         AccessDenied: 'Dostęp zabroniony.',
+        EmailDoesntExist: 'Podany email nie istnieje.',
+        InvalidData: 'Niepoprawne dane.',
         UnauthorizeAccess: 'Nieautoryzowany dostęp.',
+        Unauthorized: 'Nieautoryzowany dostęp.',
+        UnconfirmedAccount: 'Konto nie zostało powtierdzone.',
+        UserDoesntExist: 'Podany użytkownik nie istnieje.',
+        UserIsRegistered: 'Użytkownik już zarejestrowany.',
         WrongPassword: 'Niepoprawny login lub hasło.',
-        EmailDoesntExist: 'Podany amail nie istnieje.',
+        WrongRegisterToken: 'Podany użytkownik nie istnieje.',
         password: {
             toShort: 'Hasło musi zawierać od 8 do 20 znaków. Hasło powinno zawierać małe i wielkie litery, cyfrę i znak specjalny.',
             confirmBeTheSame: 'Podane hasła muszą być takie same.',
+        },
+        message: {
+            DataHasBeenSaved: 'Dane zostały zapisane.',
+            DataConfirmed: 'Dane potwierdzone.',
+            PasswordChanged: 'Hasło zmienione.',
+        },
+        Db: {
+            DuplicateEntry: 'Zduplikowana wartość: ',
         }
     }
 }
