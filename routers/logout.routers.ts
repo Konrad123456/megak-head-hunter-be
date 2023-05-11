e9591ecc70296b553cef97eb9c098f5a9ebb3072
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-05-11 08:28:15 +0200

    Fixed logout.

--- routers/logout.routers.ts
+++ routers/logout.routers.ts
@@ -7,7 +7,7 @@ export const logoutRouters = Router().post('/', async (req, res) => {
 
   if (!refreshToken) return res.sendStatus(204);
 
-  await myDataSource.getRepository(User).update({ token: refreshToken }, { token: '' });
+  await myDataSource.getRepository(User).update({ token: refreshToken }, { token: null });
 
   res.clearCookie('refreshToken');
   res.sendStatus(204);
